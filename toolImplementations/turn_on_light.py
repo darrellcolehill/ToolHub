7fc18455cde96e88c0f0f1c6dbfbf8ad6e1bd1e2
Darrell Cole Hill <darrellcolehill@gmail.com>
2025-02-15 13:58:51 -0500

    Added initial tool implementations

---
+++ toolImplementations/turn_on_light.py
@@ -0,0 +1,8 @@
+import sys
+
+
+print("Set alarm")
+
+print("Arguments passed to the script:")
+for i, arg in enumerate(sys.argv):
+    print(f"Arg {i}: {arg}")
--- toolImplementations/turn_on_lights.py
+++
@@ -1,2 +0,0 @@
-
-print("Turning on lights")
